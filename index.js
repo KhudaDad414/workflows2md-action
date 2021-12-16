ade8c01e0e816f00c1bec482beb2e8537bbba9ec
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2021-12-16 18:52:54 +0500

    join path to workspace

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core');
 const { getWorkflowsMarkdown, writeFile } = require('workflows-to-markdown');
+const path = require('path');
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const input = core.getInput('input') || '.github/workflows';
     const output = core.getInput('output') || '.github/README.md';
-    const workflows = getWorkflowsMarkdown(input);
-    writeFile(output, workflows);
+    const workflows = getWorkflowsMarkdown(
+      path.join(process.env.GITHUB_WORKSPACE, input)
+    );
+    writeFile(path.join(process.env.GITHUB_WORKSPACE, output), workflows);
+    core.setOutput('path', output);
   } catch (error) {
     core.setFailed(error.message);
   }
